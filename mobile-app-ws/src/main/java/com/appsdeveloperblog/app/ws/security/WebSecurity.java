5542fc9218b18be7fe68746c90fa8c316b437680
Vaibhav <vaibhsin@adobe.com>
2020-12-10 11:06:01 +0530

    Authentication Filter changes

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -22,8 +22,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().
-		antMatchers(HttpMethod.POST, "/users").permitAll()
-		.anyRequest().authenticated();
+		antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+		.anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
 	}
 	
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.appsdeveloperblog.app.ws.service.impl;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,39 +18,41 @@ import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@Autowired
 	Utils utils;
-	
+
 	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
 
-
 	public UserDto createUser(UserDto user) {
-		
-		if(userRepository.findByEmail(user.getEmail())!= null) throw new RuntimeException("Record already exists");
-			
-			UserEntity userEntity = new UserEntity();
-			BeanUtils.copyProperties(user, userEntity);
-			
-			userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-			userEntity.setUserId(utils.generateUserID(30));
-			
-			UserEntity storedUserDetails = userRepository.save(userEntity);
-			
-			UserDto returnVal = new UserDto();
-			BeanUtils.copyProperties(storedUserDetails, returnVal);
-			
-			return returnVal;
+
+		if (userRepository.findByEmail(user.getEmail()) != null)
+			throw new RuntimeException("Record already exists");
+
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(user, userEntity);
+
+		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		userEntity.setUserId(utils.generateUserID(30));
+
+		UserEntity storedUserDetails = userRepository.save(userEntity);
+
+		UserDto returnVal = new UserDto();
+		BeanUtils.copyProperties(storedUserDetails, returnVal);
+
+		return returnVal;
 	}
 
-
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		UserEntity userEntity = userRepository.findByEmail(email);
+
+		if (userEntity == null)
+			throw new UsernameNotFoundException(email);
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
 	}
 }
